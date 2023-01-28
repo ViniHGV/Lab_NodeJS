20d1b99a247ca59bf716c21fad45579d85d25cfc
Vini <ixxvinicius@gmail.com>
2023-01-28 18:28:40 -0300

    Implementando models

--- src/routes/models/checklist.js
+++ src/routes/models/checklist.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 const checkListSchema = mongoose.Schema({
-    name:{type: String, require: true}
+    name:{type: String, require: true},
+    tasks: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Task'
+    }]
 })
 
 module.exports = mongoose.model('Checklist', checkListSchema);
--- src/routes/models/task.js
+++ src/routes/models/task.js
@@ -1,7 +1,13 @@
 const mongoose = require('mongoose')
 
 const taskSchema = mongoose.Schema({
-    name:{type: String, require: true}
+    name:{type: String, require: true},
+    done: {type: Boolean, default:false},
+    ckeckList:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'CheckList',
+        require: true
+    }
 })
 
 module.exports = mongoose.model('Checklist', taskSchema);
